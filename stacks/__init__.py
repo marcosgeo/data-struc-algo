2412f069a8b1bdbc9548476e599733031413af8e
Antonio Marcos <marcosgeo@yahoo.com>
2024-12-27 18:47:21 -0300

    added three essential methods to SimpleStack class

--- stacks/__init__.py
+++ stacks/__init__.py
@@ -6,3 +6,15 @@ class BasicStack:
     def __init__(self):
         self.list = []
 
+    def __str__(self):
+        values = [str(x) for x in reversed(self.list)]
+        return "\n".join(values)
+
+    def push(self, item):
+        """Add an item to the stack."""
+        self.list.append(item)
+        return self
+
+    def isempty(self):
+        """Check if the stack is empty."""
+        return len(self.list) == 0
--- stacks/test_stacks.py
+++ stacks/test_stacks.py
@@ -7,3 +7,37 @@ from . import BasicStack
 def test_stack_init():
     stack = BasicStack()
     assert stack.list == []
+
+
+@pytest.mark.parametrize("items, expected", [
+    ([1], [1]),
+    ([1, 2], [1, 2]),
+    ([1, 2, 3], [1, 2, 3]),
+])
+def test_stack_push(items, expected):
+    stack = BasicStack()
+    for item in items:
+        stack.push(item)
+    assert stack.list == expected
+
+
+def test_stack_str():
+    stack = BasicStack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    print()
+    print(stack)
+    assert str(stack) == "3\n2\n1"
+
+
+@pytest.mark.parametrize("items, expected", [
+    ([], True),
+    ([1], False),
+    ([1, 2], False),
+])
+def test_stack_isempty(items, expected):
+    stack = BasicStack()
+    for item in items:
+        stack.push(item)
+    assert stack.isempty() == expected
