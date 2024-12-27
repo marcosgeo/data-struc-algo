5cf1bf7642abe15fe27404b91eb5db57b5a4b2af
Antonio Marcos <marcosgeo@yahoo.com>
2024-12-27 17:24:20 -0300

    added append method

--- circular/cslinkedlist.py
+++ circular/cslinkedlist.py
@@ -10,9 +10,19 @@ class Node:
 
 
 class CSLinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        new_node.next = new_node
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node: Node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+        else:
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.tail = new_node
+        self.length += 1
--- circular/test_cslinkedlist.py
+++ circular/test_cslinkedlist.py
@@ -3,13 +3,26 @@ import pytest
 from circular.cslinkedlist import CSLinkedList
 
 
-@pytest.mark.parametrize("value, head, tail, next_", [
-    (1, 1, 1, 1),
-    (2, 2, 2, 2,),
+@pytest.mark.parametrize("value, head, tail", [
+    (1, 1, 1),
+    (2, 2, 2,),
+    (2, 5, 6),
 ])
-def test_cslinkedlist_init(value, head, tail, next_):
-    csll = CSLinkedList(value)
+def test_cslinkedlist_init(value, head, tail):
+    csll = CSLinkedList()
+    csll.append(head)
+    csll.append(tail)
     assert csll.head.value == head
     assert csll.tail.value == tail
-    assert csll.head.next.value == next_
 
+
+@pytest.mark.parametrize("list1, expected", [
+    ([1, 2, 3], [1, 2, 3]),
+    ([4, 5, 6], [4, 5, 6]),
+])
+def test_cslinkedlist_append(list1, expected):
+    csll = CSLinkedList()
+    for value in list1:
+        csll.append(value)
+    assert csll.head.value == expected[0]
+    assert csll.tail.value == expected[-1]
