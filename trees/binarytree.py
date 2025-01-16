f463a18d387ae2081377db6772e1deaa062b58df
Antonio Marcos <marcosgeo@yahoo.com>
2025-01-16 10:43:12 -0300

    added pre_order_traversal function to traverse a tree

--- trees/binarytree.py
+++ trees/binarytree.py
@@ -8,3 +8,13 @@ class TreeNode:
         self.data = data
         self.left_child = None
         self.right_child = None
+
+
+def pre_order_traversal(root_node: TreeNode, result=[]):
+    if not root_node:
+        return
+    print(root_node.data)
+    result.append(root_node.data)
+    pre_order_traversal(root_node.left_child, result)
+    pre_order_traversal(root_node.right_child, result)
+    return result
--- trees/binarytree_test.py
+++ trees/binarytree_test.py
@@ -1,4 +1,4 @@
-from .binarytree import TreeNode
+from .binarytree import TreeNode, pre_order_traversal
 
 
 def test_should_create_root_node():
@@ -6,3 +6,18 @@ def test_should_create_root_node():
     assert root.data == "trunk"
     assert root.left_child is None
     assert root.right_child is None
+
+
+def test_should_traverse_tree_in_pre_order():
+    root = TreeNode("\ntrunk")
+    left = TreeNode("left")
+    right = TreeNode("right")
+    root.left_child = left
+    root.right_child = right
+    left.left_child = TreeNode("left-left")
+    left.right_child = TreeNode("left-right")
+    right.left_child = TreeNode("right-left")
+    right.right_child = TreeNode("right-right")
+    result = pre_order_traversal(root)
+    assert result == ["\ntrunk", "left", "left-left", "left-right", "right", "right-left", "right-right"]
+
